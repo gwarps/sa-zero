0a637c0a7eb89d686e69d61772f5bd66a161d8a1
rbc <rinkesh@rbc.(none)>
2012-10-08 04:40:52 +0530

    part3 completed

--- part3.rb
+++ part3.rb
@@ -1,3 +1,13 @@
 def combine_anagrams(words)
  # YOUR CODE HERE
+ hash = Hash.new
+ words.each do |word|
+  temp = word.downcase.chars.sort.join
+  if hash[temp].nil?
+   hash[temp] = Array.new
+  end
+  hash[temp].push word
+ end
+ return hash.values
 end
+
