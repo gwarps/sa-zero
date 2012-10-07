e3b285f8238ed3c37ca811c8210855adef608a5e
rbc <rinkesh@rbc.(none)>
2012-10-08 05:14:53 +0530

    part4 complete

--- part4.rb
+++ part4.rb
@@ -1,23 +1,35 @@
 class Dessert
+  attr_accessor :name, :calories
   def initialize(name, calories)
     # YOUR CODE HERE
+    @name = name
+    @calories = calories
   end
   
   def healthy?
     # YOUR CODE HERE
+    return @calories < 200
   end
   
   def delicious?
     # YOUR CODE HERE
+    return true
   end
 end
 
 class JellyBean < Dessert
+  attr_accessor :flavor
   def initialize(name, calories, flavor)
     # YOUR CODE HERE
+    #@name = name
+    #@calories = calories
+    super(name,calories)
+    @flavor = flavor
   end
   
   def delicious?
     # YOUR CODE HERE
+    return false if @flavor == "black licorice"
+    super
   end
 end
