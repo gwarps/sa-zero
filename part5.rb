734efaf2cb089ff1f1208f8b3676a1a44e060a1f
Puneet Singh <puneet.mir@gmail.com>
2012-10-12 00:28:23 +0530

    part5 completed

--- part5.rb
+++ part5.rb
@@ -3,6 +3,18 @@ class Class
     attr_name = attr_name.to_s   # make sure it's a string
     attr_reader attr_name        # create the attribute's getter
     attr_reader attr_name+"_history" # create bar_history getter
-    class_eval "your code here, use %Q for multiline strings"
+#class_eval "your code here, use %Q for multiline strings"
+    class_eval %Q{
+      def #{attr_name}=(value)
+        if @#{attr_name}_history.nil?
+          @#{attr_name}_history = Array.new
+          @#{attr_name}_history.push(nil)
+        else
+          @#{attr_name} = value
+        end
+        @#{attr_name} = value
+        @#{attr_name}_history.push(value)
+      end
+    }
   end
 end
